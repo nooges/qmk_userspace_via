4f014aa9d3f0a8e7f627c937572a088c473e4ad5
Danny Nguyen <danny@keeb.io>
2026-02-04 10:37:51 -0500

    Set default RGB mode and speed for Demod LM

---
+++ keyboards/keebio/demod_lm/keymaps/via/config.h
@@ -0,0 +1,7 @@
+// Copyright 2026 Keebio (@keebio)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CYCLE_PINWHEEL
+#define RGB_MATRIX_DEFAULT_SPD 48
